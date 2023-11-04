502ec0613c37366114f8e1095b51d09ce22b3e9c
siyeol97 <leesi2830@naver.com>
2023-11-04 13:28:12 +0900

    11/03

---
+++ src/routes/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login(){
+    return (
+        <>
+            <h1>This is Login component</h1>
+        </>
+    )
+}
---
+++ src/routes/Profile.tsx
@@ -0,0 +1,7 @@
+export default function Profile(){
+    return (
+        <>
+            <h1>This is Profile component</h1>
+        </>
+    )
+}
